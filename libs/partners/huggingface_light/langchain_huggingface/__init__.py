9cb0466c71a414c0b00e2fa956386b842c274cbf
Morgan Diverrez <morgan.diverrez@arkea.com>
2024-09-04 14:54:17 +0200

    first test

---
+++ libs/partners/huggingface_light/langchain_huggingface/__init__.py
@@ -0,0 +1,11 @@
+from langchain_huggingface.embeddings import (
+    HuggingFaceEndpointEmbeddings,
+)
+from langchain_huggingface.llms import (
+    HuggingFaceEndpoint,
+)
+
+__all__ = [
+    "HuggingFaceEndpointEmbeddings",
+    "HuggingFaceEndpoint",
+]
